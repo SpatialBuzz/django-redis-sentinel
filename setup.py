7ad3a21fe467bfa9bd268594d386a113a9116913
Chris Heisel <cheisel@kabbage.com>
2015-04-19 17:13:15 -0400

    Initial import of SentinelClient

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+description = """
+Plugin for django-redis that supports Redis Sentinel
+"""
+
+setup(
+    name="django-redis-sentinel",
+    url="https://github.com/KabbageInc/django-redis-sentinel",
+    author="Chris Heisel",
+    author_email="cheisel@kabbage.com",
+    version="1.0-rc1",
+    packages=[
+        "django_redis_sentinel",
+    ],
+    description=description.strip(),
+    install_requires=[
+        "django-redis==3.8.3",
+        "Django==1.8",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    package_data={
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
